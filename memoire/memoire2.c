c9b0e759398e9be9a5fbf1b20512b8428c9cbc12
Maruku <73369025+MarukuSan@users.noreply.github.com>
2022-05-07 01:45:41 +0200

    Add files via upload

--- memoire/memoire2.c
+++ memoire/memoire2.c
@@ -30,21 +30,87 @@ memoire creerMemoire (int tailleMot, int nbMots) {
   return m;
 }
 
+/* Calcule du nombre de mots libres dans la mémoire */
+int nbreMotLibre(memoire m) {
+  Element* courant;
+  courant = m.listBlocs->premier;
+  int total = 0;
+  while (courant != NULL) {
+    if (courant->data.drapeau == Libre) {
+      total = total + courant->data.taille;
+    }
+    courant = courant->suivant;
+  }
+  return total;
+}
 
-/* bloc allocationMemoire (memoire mem, int tailleAAllouer) {
 
+bloc allocationMemoire (memoire mem, int tailleAAllouer) {
+  bloc aAjouter;
+  if (tailleAAllouer > mem.nbMots || tailleAAllouer > nbreMotLibre(mem)) {
+    printf("Impossible d'allouer : Taille demandee superieur a la taille disponible\n");
+    exit(EXIT_FAILURE);
+  }
+  else {
+    Element* courant;
+    courant = mem.listBlocs->premier;
+    int i = 0;
+
+    while (courant != NULL) {
+      if (courant->data.drapeau == Libre && courant->data.taille >= tailleAAllouer) {
+        aAjouter.debut = courant->data.debut;
+        aAjouter.taille = tailleAAllouer;
+        aAjouter.drapeau = Utilise;
+        courant->data.debut = aAjouter.debut + aAjouter.taille;
+        courant->data.taille = courant->data.taille - aAjouter.taille;
+        insertion2(mem.listBlocs, aAjouter, i);
+      }
+      i++;
+      courant = courant->suivant;
+    }
+  }
+  return aAjouter;
 }
 
 void liberationMemoire (memoire mem, bloc blocALiberer) {
-  
-} */
+  Element* courant = mem.listBlocs->premier;
+  int i = 0;
+  while (courant != NULL) {
+    if (courant->data.debut == blocALiberer.debut && courant->data.taille == blocALiberer.taille) {
+      courant->data.drapeau = Libre;
+      /* Fusionne avec le bloc suivant s'il est libre */
+      if (courant->suivant->data.drapeau == Libre) {
+        courant->data.taille = courant->data.taille + courant->suivant->data.taille;
+        suppression2(mem.listBlocs, i+1);
+      }
+    }
+    
+    i++;
+    courant = courant->suivant;
+  }  
+}
 
 int main() {
-  memoire a = creerMemoire(2, 32);
-
+  memoire a = creerMemoire(2, 264);
   afficherListe(a.listBlocs);
+  printf("%d\n", nbreMotLibre(a));
 
-   
+  allocationMemoire(a, 4);
+  afficherListe(a.listBlocs);
+  printf("%d\n", nbreMotLibre(a));
+
+  allocationMemoire(a, 32);
+  afficherListe(a.listBlocs);
+  printf("%d\n", nbreMotLibre(a));
+
+  allocationMemoire(a, 12);
+  afficherListe(a.listBlocs);
+  printf("%d\n", nbreMotLibre(a));
+
+  bloc b = {36, 12, Libre};
+  liberationMemoire(a, b);
+  afficherListe(a.listBlocs);
+  printf("%d\n", nbreMotLibre(a));
 
   return 0;
 }
